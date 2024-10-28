4a40b3a2c562d6faec221d39322406a92ff328a2
dhlotus <phucducha12@gmail.com>
2024-10-28 21:39:36 +0700

    s

--- ha.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("keydown", function(event) {
-    if (event.key === "F12") {
-        event.preventDefault();
-    }
-});
-document.addEventListener("contextmenu", function(event) {
-    event.preventDefault();
-});
