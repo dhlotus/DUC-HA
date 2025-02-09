1169d25386b945e25af447140a3fb50743de5b61
dhlotus <phucducha12@gmail.com>
2025-02-09 21:39:22 +0700

    cập nhật ++

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 
 
-// document.addEventListener('contextmenu', function(event) {
-//     event.preventDefault();
-// });
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+});
 
-// document.addEventListener('keydown', function(event) {
-//     if (event.key === "F12" || 
-//         (event.ctrlKey && event.shiftKey && event.key === "I") ||  
-//         (event.ctrlKey && event.shiftKey && event.key === "J") ||  
-//         (event.ctrlKey && event.key === "U") 
-//        ) {
-//         event.preventDefault(); 
-//     }
-// });
-// // u
-// document.addEventListener('keydown', function (event) {
-// if (event.ctrlKey && event.key.toLowerCase() === 'u') {
-//     event.preventDefault(); 
-//     return false; 
-// }
-// });
+document.addEventListener('keydown', function(event) {
+    if (event.key === "F12" || 
+        (event.ctrlKey && event.shiftKey && event.key === "I") ||  
+        (event.ctrlKey && event.shiftKey && event.key === "J") ||  
+        (event.ctrlKey && event.key === "U") 
+       ) {
+        event.preventDefault(); 
+    }
+});
+// u
+document.addEventListener('keydown', function (event) {
+if (event.ctrlKey && event.key.toLowerCase() === 'u') {
+    event.preventDefault(); 
+    return false; 
+}
+});
 particlesJS("particles-js", {
     "particles": {
         "number": {"value": 40},
