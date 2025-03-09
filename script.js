4f58bcce5b84c06ac02378d3e2b1c7fd77c2b8ea
dhlotus <phucducha12@gmail.com>
2025-03-09 16:43:08 +0700

    update mussic

--- script.js
+++ script.js
@@ -158,23 +158,23 @@ function activateOnScroll() {
   });
 }
 
-// document.addEventListener('contextmenu', function(event) {
-//     event.preventDefault();
-// });
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+});
 
-// document.addEventListener('keydown', function(event) {
-//     if (event.key === "F12" || 
-//         (event.ctrlKey && event.shiftKey && event.key === "I") ||  
-//         (event.ctrlKey && event.shiftKey && event.key === "J") ||  
-//         (event.ctrlKey && event.key === "U") 
-//        ) {
-//         event.preventDefault(); 
-//     }
-// });
-// // u
-// document.addEventListener('keydown', function (event) {
-// if (event.ctrlKey && event.key.toLowerCase() === 'u') {
-//     event.preventDefault(); 
-//     return false; 
-// }
-// });
+document.addEventListener('keydown', function(event) {
+    if (event.key === "F12" || 
+        (event.ctrlKey && event.shiftKey && event.key === "I") ||  
+        (event.ctrlKey && event.shiftKey && event.key === "J") ||  
+        (event.ctrlKey && event.key === "U") 
+       ) {
+        event.preventDefault(); 
+    }
+});
+// u
+document.addEventListener('keydown', function (event) {
+if (event.ctrlKey && event.key.toLowerCase() === 'u') {
+    event.preventDefault(); 
+    return false; 
+}
+});
